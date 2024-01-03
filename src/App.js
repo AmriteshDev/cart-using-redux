e1c6c712b1e021c3d1c4f901639cf0cca13e149c
Amriteshsingh94 <amriteshsingh2000n@gmail.com>
2024-01-03 18:25:05 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
-
+import Header from "./components/Header";
+import Home from "./components/Home";
+import "./styles/app.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div>nv</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiShoppingBag } from "react-icons/fi";
+const Header = () => {
+    return (
+        <nav>
+            <h2>Logo Here</h2>
+            <div>
+                <Link to={"/"}> Home</Link>
+                <Link to={"/cart"}>
+                    <FiShoppingBag />
+                    <p>{0}</p>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
